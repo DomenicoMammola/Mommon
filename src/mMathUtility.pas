1519800a6c10646839db7d8e6fe92e338f51542f
Domenico Mammola <mimmo71@gmail.com>
2017-05-03 16:53:33 +0200

    * revisited functions

--- src/mMathUtility.pas
+++ src/mMathUtility.pas
@@ -12,31 +12,43 @@ unit mMathUtility;
 
 interface
 
-// CREDITS: https://rosettacode.org/wiki/Determine_if_a_string_is_numeric#Pascal
-function IsNumeric(Value: string; const AllowFloat: Boolean): Boolean;
+function TryToConvertToDouble(aValue : string; var aOutValue : Double ): boolean;
+function IsNumeric(aValue: string; const aAllowFloat: Boolean): Boolean;
 
 implementation
 
 uses
   SysUtils;
 
-// CREDITS: https://rosettacode.org/wiki/Determine_if_a_string_is_numeric#Pascal
-function IsNumeric(Value: string; const AllowFloat: Boolean): Boolean;
+function TryToConvertToDouble(aValue : string; var aOutValue : Double): boolean;
 var
-  ValueInt: Integer;
-  ValueFloat: Extended;
-  ErrCode: Integer;
+  tmpValueDouble : double;
+  tmp : String;
 begin
-// Check for integer: Val only accepts integers when passed integer param
-Value := SysUtils.Trim(Value);
-Val(Value, ValueInt, ErrCode);
-Result := ErrCode = 0;      // Val sets error code 0 if OK
-if not Result and AllowFloat then
-    begin
-    // Check for float: Val accepts floats when passed float param
-    Val(Value, ValueFloat, ErrCode);
-    Result := ErrCode = 0;    // Val sets error code 0 if OK
-    end;
+  Result := TryStrToFloat(aValue, tmpValueDouble);
+  if Result then
+    aOutValue := tmpValueDouble
+  else
+  begin
+    if SysUtils.FormatSettings.DecimalSeparator = '.' then
+      tmp := StringReplace(aValue, ',', '.', [rfReplaceAll])
+    else
+      tmp := StringReplace(aValue, '.', ',', [rfReplaceAll]);
+    Result := TryStrToFloat(tmp, tmpValueDouble);
+    if Result then
+      aOutValue := tmpValueDouble
+  end;
+end;
+
+function IsNumeric(aValue: string; const aAllowFloat: Boolean): Boolean;
+var
+  tmpInt : integer;
+  tmpDouble : double;
+begin
+  if aAllowFloat then
+    Result := TryToConvertToDouble(aValue, tmpDouble)
+  else
+    Result := TryStrToInt(aValue, tmpInt);
 end;
 
 end.
