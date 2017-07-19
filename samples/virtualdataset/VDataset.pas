ab207d2580242d40a6163bf69253d7f9bbb83926
Domenico Mammola <mimmo71@gmail.com>
2017-07-19 18:17:09 +0200

    * sample project moved from OboCL to Mommon

---
+++ samples/virtualdataset/VDataset.pas
@@ -0,0 +1,151 @@
+unit VDataset;
+
+{$IFDEF FPC}
+  {$MODE DELPHI}
+{$ENDIF}
+
+interface
+
+uses
+  Classes, DB,
+  contnrs,
+  mVirtualDataSet, mVirtualFieldDefs;
+
+type
+
+  TCiccio = class
+  private
+    FVString : string;
+    FVDouble : double;
+    FVInteger : integer;
+  public
+  published
+    property ValueString : string read FVString write FVString;
+    property ValueFloat : double read FVDouble write FVDouble;
+    property ValueInteger : integer read FVInteger write FVInteger;
+  end;
+
+
+  { TListVirtualDatasetDataProvider }
+
+  TListVirtualDatasetDataProvider = class (TVirtualDatasetDataProvider)
+  private
+    FList : TList;
+    FGarbageCollector : TObjectList;
+  public
+    constructor Create; override;
+    destructor Destroy; override;
+    procedure Add (AObj : TObject);
+
+    procedure GetFieldValue (const AField: TField; const AIndex: Integer;
+      out AValue: variant); override;
+    procedure DeleteRecord (const AIndex :integer); override;
+    procedure EditRecord (const AIndex : integer; AModifiedFields : TList); override;
+    procedure InsertRecord (const AIndex : integer; AModifiedFields : TList); override;
+    function GetRecordCount : integer; override;
+  end;
+
+implementation
+
+{ TListVirtualDatasetDataProvider }
+
+constructor TListVirtualDatasetDataProvider.Create;
+begin
+  inherited;
+  FList := TList.Create;
+  FGarbageCollector:= TObjectList.Create(true);;
+  with Self.VirtualFieldDefs.AddFieldDef do
+  begin
+    Name := 'ValueString';
+    DataType := vftString;
+    Size := 30;
+  end;
+  with Self.VirtualFieldDefs.AddFieldDef do
+  begin
+    Name := 'ValueInteger';
+    DataType := vftInteger;
+  end;
+  with Self.VirtualFieldDefs.AddFieldDef do
+  begin
+    Name := 'ValueFloat';
+    DataType := vftFloat;
+  end;
+end;
+
+procedure TListVirtualDatasetDataProvider.DeleteRecord(const AIndex :integer);
+begin
+  //
+end;
+
+destructor TListVirtualDatasetDataProvider.Destroy;
+begin
+  FList.Free;
+  FGarbageCollector.Free;
+  inherited;
+end;
+
+procedure TListVirtualDatasetDataProvider.Add(AObj: TObject);
+begin
+  FList.Add(AObj);
+  FGarbageCollector.Add(AObj);
+end;
+
+procedure TListVirtualDatasetDataProvider.EditRecord(const AIndex : integer; AModifiedFields : TList);
+var
+  O : TCiccio;
+  i : integer;
+  F : TField;
+begin
+  O := TObject(FList[AIndex]) as TCiccio;
+  for i := 0 to AModifiedFields.Count - 1 do
+  begin
+    F := TObject(AModifiedFields.Items[i]) as TField;
+    if F.FieldName = 'ValueString' then
+    begin
+      O.ValueString := F.AsString;
+    end
+    else
+    if F.FieldName = 'ValueInteger' then
+    begin
+      O.ValueInteger := F.AsInteger;
+    end
+    else
+    begin
+      O.ValueFloat := F.AsFloat;
+    end;
+  end;
+
+end;
+
+procedure TListVirtualDatasetDataProvider.GetFieldValue(const AField: TField; const AIndex: Integer;
+      out AValue: variant);
+var
+  AObject : TCiccio;
+begin
+  if AIndex >= 0 then
+  begin
+    AObject := TCiccio(FList.Items[AIndex]);
+    if AField.FieldName = 'ValueString' then
+      AValue := (AObject as TCiccio).ValueString
+    else
+    if AField.FieldName = 'ValueInteger' then
+      AValue := (AObject as TCiccio).ValueInteger
+    else
+    if AField.FieldName = 'ValueFloat' then
+      AValue := (AObject as TCiccio).ValueFloat;
+  end;
+end;
+
+function TListVirtualDatasetDataProvider.GetRecordCount: integer;
+begin
+  Result := FList.Count;
+end;
+
+procedure TListVirtualDatasetDataProvider.InsertRecord(const AIndex : integer; AModifiedFields : TList);
+begin
+  //
+
+end;
+
+
+end.
