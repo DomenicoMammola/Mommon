3196698f8bb31aa312de66679cdf5e83f6463227
Domenico Mammola <mimmo71@gmail.com>
2017-10-22 08:25:33 +0200

    * no more cast between signed nativeint and pointers

--- src/mIntList.pas
+++ src/mIntList.pas
@@ -13,55 +13,61 @@ unit mIntList;
 interface
 
 uses Classes
-{$IFNDEF FPC}
+{$IFDEF FPC}
+  , fgl
+{$ELSE}
   ,System.Types
 {$ENDIF};
 
 type
 
-  // MUST CONVERT TO PtrUint
+  {$IFDEF FPC}
+  mUnsignedInt = PtrUInt;
+  {$ELSE}
+  mUnsignedInt = cardinal;
+  {$ENDIF}
 
-  { A list of integer}
-  TCustomIntegerList = class(TList)
+  TCustomCardinalList = class(TList)
   protected
-    function Get(Index: Integer): NativeInt; virtual;
-    procedure Put(Index: Integer; const Value: NativeInt);
-    procedure Inst(Index : Integer; Num: NativeInt);
+    function Get(Index: Integer): mUnsignedInt; virtual;
+    procedure Put(Index: Integer; const Value: mUnsignedInt);
+    procedure Inst(Index : Integer; Num: mUnsignedInt);
   public
     function GetText : string;
-    function Add(Num: NativeInt): Integer; virtual;
-    procedure AddIntegers (IntegerList : TCustomIntegerList);
-    function First: NativeInt;
-    function IndexOf(Num: NativeInt): Integer; virtual;
-    function Last: NativeInt;
-    function Remove(Num: NativeInt): Integer;
-    procedure Pack(RemoveNum: NativeInt);
+    function Add(Num: mUnsignedInt): Integer; virtual;
+    procedure AddIntegers (IntegerList : TCustomCardinalList);
+    function First: mUnsignedInt;
+    function IndexOf(Num: mUnsignedInt): Integer; virtual;
+    function Last: mUnsignedInt;
+    function Remove(Num: mUnsignedInt): Integer;
+    procedure Pack(RemoveNum: mUnsignedInt);
     procedure Sort; virtual;
     procedure SaveToFile (const FileName : string);
     procedure SaveToStream (Stream: TStream);
 
-    property Items[Index: Integer]: NativeInt read Get write Put; default;
-    property Nums[Index: Integer]: NativeInt read Get write Put;
+    property Items[Index: Integer]: mUnsignedInt read Get write Put; default;
+    property Nums[Index: Integer]: mUnsignedInt read Get write Put;
   end;
 
-  TIntegerList = class (TCustomIntegerList)
+  TCardinalList = class (TCustomCardinalList)
   public
-    procedure Insert(Index: integer; Num: NativeInt);
+    procedure Insert(Index: integer; Num: mUnsignedInt);
   end;
 
-
-  TSortedIntegerList = class (TCustomIntegerList)
+  TSortedCardinalList = class (TCustomCardinalList)
   private
     FSorted : boolean;
-    function InternalGet(Index: Integer): NativeInt;
+    function InternalGet(Index: Integer): mUnsignedInt;
   protected
-    function Get(Index: Integer): NativeInt; override;
+    function Get(Index: Integer): mUnsignedInt; override;
     procedure Notify(Ptr: Pointer; Action: TListNotification); override;
   public
-    function IndexOf(Num: NativeInt): Integer; override;
+    function IndexOf(Num: mUnsignedInt): Integer; override;
     procedure Sort; override;
-    procedure AddSorted(Num: NativeInt);
+    procedure AddSorted(Num: mUnsignedInt);
   end;
+
+  TIntegerList = specialize TFPGList<Integer>;
 
 
 implementation
@@ -71,17 +77,17 @@ uses
 
 function SortBySmall(Item1, Item2: Pointer): Integer;
 begin
-  if NativeInt( Item1 )>NativeInt( Item2 ) then Result := 1
-    else if NativeInt( Item1 )<NativeInt( Item2 ) then Result := -1
+  if mUnsignedInt( Item1 )>mUnsignedInt( Item2 ) then Result := 1
+    else if mUnsignedInt( Item1 )<mUnsignedInt( Item2 ) then Result := -1
       else Result := 0;
 end;
 
-function TCustomIntegerList.Get(Index: Integer): NativeInt;
+function TCustomCardinalList.Get(Index: Integer): mUnsignedInt;
 begin
-  Result := NativeInt( inherited Get( Index ) );
+  Result := mUnsignedInt( inherited Get( Index ) );
 end;
 
-function TCustomIntegerList.GetText: string;
+function TCustomCardinalList.GetText: string;
 var
   i : integer;
 begin
@@ -95,42 +101,42 @@ begin
   end;
 end;
 
-procedure TCustomIntegerList.Put(Index: Integer; const Value: NativeInt);
+procedure TCustomCardinalList.Put(Index: Integer; const Value: mUnsignedInt);
 begin
   inherited Put( Index, Pointer( Value ) );
 end;
 
-function TCustomIntegerList.Add(Num: NativeInt): Integer;
+function TCustomCardinalList.Add(Num: mUnsignedInt): Integer;
 begin
-  Result := inherited Add( Pointer(Num ) );
+  Result := inherited Add( pointer(Num) );
 end;
 
-function TCustomIntegerList.First: NativeInt;
+function TCustomCardinalList.First: mUnsignedInt;
 begin
   Result := Get( 0 );
 end;
 
-function TCustomIntegerList.IndexOf(Num: NativeInt): Integer;
+function TCustomCardinalList.IndexOf(Num: mUnsignedInt): Integer;
 begin
   Result := inherited IndexOf( Pointer( Num ) );
 end;
 
-procedure TCustomIntegerList.Inst(Index: Integer; Num : NativeInt);
+procedure TCustomCardinalList.Inst(Index: Integer; Num : mUnsignedInt);
 begin
   inherited Insert( Index, Pointer( Num ) );
 end;
 
-function TCustomIntegerList.Last: NativeInt;
+function TCustomCardinalList.Last: mUnsignedInt;
 begin
   Result := Get( Count-1 );
 end;
 
-function TCustomIntegerList.Remove(Num: NativeInt): Integer;
+function TCustomCardinalList.Remove(Num: mUnsignedInt): Integer;
 begin
    Result := inherited Remove( Pointer( Num ) );
 end;
 
-procedure TCustomIntegerList.Pack(RemoveNum: NativeInt);
+procedure TCustomCardinalList.Pack(RemoveNum: mUnsignedInt);
 var
   idx : Integer;
 begin
@@ -141,7 +147,7 @@ begin
   end;
 end;
 
-procedure TCustomIntegerList.SaveToFile(const FileName: string);
+procedure TCustomCardinalList.SaveToFile(const FileName: string);
 var
   Stream: TStream;
 begin
@@ -153,7 +159,7 @@ begin
   end;
 end;
 
-procedure TCustomIntegerList.SaveToStream(Stream: TStream);
+procedure TCustomCardinalList.SaveToStream(Stream: TStream);
 var
   S: string;
 begin
@@ -161,12 +167,12 @@ begin
   Stream.WriteBuffer(Pointer(S)^, Length(S));
 end;
 
-procedure TCustomIntegerList.Sort;
+procedure TCustomCardinalList.Sort;
 begin
   inherited Sort({$IFDEF FPC}@{$ENDIF}SortBySmall );
 end;
 
-procedure TCustomIntegerList.AddIntegers(IntegerList: TCustomIntegerList);
+procedure TCustomCardinalList.AddIntegers(IntegerList: TCustomCardinalList);
 var
   ICounter : integer;
 begin
@@ -176,9 +182,9 @@ begin
   end;
 end;
 
-{ TSortedIntegerList }
+{ TSortedCardinalList }
 
-procedure TSortedIntegerList.AddSorted(Num: NativeInt);
+procedure TSortedCardinalList.AddSorted(Num: mUnsignedInt);
   procedure InsertInto (aStart, aEnd : integer);
   var
     tempValue, midIndex : integer;
@@ -218,14 +224,14 @@ begin
 end;
 
 
-function TSortedIntegerList.Get(Index: Integer): NativeInt;
+function TSortedCardinalList.Get(Index: Integer): mUnsignedInt;
 begin
   if not FSorted then
     Self.Sort;
   Result := InternalGet(Index);
 end;
 
-function TSortedIntegerList.IndexOf(Num: NativeInt): Integer;
+function TSortedCardinalList.IndexOf(Num: mUnsignedInt): Integer;
   function MidIndexOf(aStart, aEnd : integer) : integer;
   var
     tempValue, midIndex : integer;
@@ -272,17 +278,17 @@ begin
   end;
 end;
 
-function TSortedIntegerList.InternalGet(Index: Integer): NativeInt;
+function TSortedCardinalList.InternalGet(Index: Integer): mUnsignedInt;
 begin
 Result := inherited Get(Index);
 end;
 
-procedure TSortedIntegerList.Notify(Ptr: Pointer; Action: TListNotification);
+procedure TSortedCardinalList.Notify(Ptr: Pointer; Action: TListNotification);
 begin
   FSorted := false;
 end;
 
-procedure TSortedIntegerList.Sort;
+procedure TSortedCardinalList.Sort;
 begin
   if FSorted then
     exit;
@@ -290,9 +296,9 @@ begin
   FSorted := true;
 end;
 
-{ TIntegerList }
+{ TCardinalList }
 
-procedure TIntegerList.Insert(Index: Integer; Num: NativeInt);
+procedure TCardinalList.Insert(Index: Integer; Num: mUnsignedInt);
 begin
   Inst(Index, Num);
 end;
