f1e7facec6d31ee82e583170d2532a6a91b3f273
Domenico Mammola <mimmo71@gmail.com>
2018-02-03 17:46:45 +0100

    * add test case for container codes utilities

--- tests/mUtility/mutilitytestcase.pas
+++ tests/mUtility/mutilitytestcase.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, fpcunit, testutils, testregistry,
-  mUtility;
+  mUtility, mISO6346Utility;
 
 type
 
@@ -16,6 +16,7 @@ type
   published
     procedure TestAddRemoveZeros;
     procedure TestGetCPUCores;
+    procedure TestISO6346;
   end;
 
 implementation
@@ -38,6 +39,27 @@ end;
 procedure TTestCaseUtility.TestGetCPUCores;
 begin
   CheckTrue(GetCPUCores > 0);
+end;
+
+procedure TTestCaseUtility.TestISO6346;
+var
+  error : string;
+begin
+  CheckTrue(IsContainerCodeValid('CSQU3054383', error));
+  CheckFalse(IsContainerCodeValid('CSQU3054381', error));
+  CheckFalse(IsContainerCodeValid('CSQU3054382', error));
+  CheckFalse(IsContainerCodeValid('CSQU3054384', error));
+  CheckFalse(IsContainerCodeValid('CSQU3054385', error));
+  CheckFalse(IsContainerCodeValid('CSQU3054386', error));
+  CheckFalse(IsContainerCodeValid('CSQU3054387', error));
+  CheckFalse(IsContainerCodeValid('CSQU3054388', error));
+  CheckFalse(IsContainerCodeValid('CSQU3054389', error));
+  CheckFalse(IsContainerCodeValid('', error));
+  CheckFalse(IsContainerCodeValid('12345678901', error));
+  CheckFalse(IsContainerCodeValid('CSQK3054383', error));
+  CheckFalse(IsContainerCodeValid('CSU3054383', error));
+  CheckFalse(IsContainerCodeValid('CS2U3054383', error));
+  CheckFalse(IsContainerCodeValid('CSQU30A4383', error));
 end;
 
 
