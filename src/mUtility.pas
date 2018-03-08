ab89068b9257b0eb79c4238f0b32e90c4bb9b431
Domenico Mammola <mimmo71@gmail.com>
2018-03-08 18:35:02 +0100

    * add function ValidEmail (by Ernesto D'Spirito http://www.howtodothings.com/computers/a1169-validating-email-addresses-in-delphi.html )

--- src/mUtility.pas
+++ src/mUtility.pas
@@ -88,6 +88,12 @@ function RegisterDefaultApplication(const aFullPathExe : string; const aFileExte
 // http://forum.lazarus.freepascal.org/index.php?topic=13705.0
 function BytesToHumanReadableString(const bytes: UInt64): string;
 
+
+// https://www.ietf.org/rfc/rfc822.txt
+// Author: Ernesto D'Spirito
+// http://www.howtodothings.com/computers/a1169-validating-email-addresses-in-delphi.html
+function ValidEmail(const email: string): boolean;
+
 implementation
 
 uses
@@ -1132,6 +1138,100 @@ begin
           result := FormatFloat('#.## bytes', bytes) ;
 end;
 
+
+function ValidEmail(const email: string): boolean;
+// Returns True if the email address is valid
+// Author: Ernesto D'Spirito
+const
+  // Valid characters in an "atom"
+  atom_chars = [#33..#255] - ['(', ')', '<', '>', '@', ',', ';', ':',
+                              '\', '/', '"', '.', '[', ']', #127];
+  // Valid characters in a "quoted-string"
+  quoted_string_chars = [#0..#255] - ['"', #13, '\'];
+
+  // Valid characters in a subdomain
+  letters = ['A'..'Z', 'a'..'z'];
+  letters_digits = ['0'..'9', 'A'..'Z', 'a'..'z'];
+  subdomain_chars = ['-', '0'..'9', 'A'..'Z', 'a'..'z'];
+type
+  States = (STATE_BEGIN, STATE_ATOM, STATE_QTEXT, STATE_QCHAR,
+    STATE_QUOTE, STATE_LOCAL_PERIOD, STATE_EXPECTING_SUBDOMAIN,
+    STATE_SUBDOMAIN, STATE_HYPHEN);
+var
+  State: States;
+  i, n, subdomains: integer;
+  c: char;
+begin
+  State := STATE_BEGIN;
+  n := Length(email);
+  i := 1;
+  subdomains := 1;
+  while (i <= n) do begin
+    c := email[i];
+    case State of
+    STATE_BEGIN:
+      if c in atom_chars then
+        State := STATE_ATOM
+      else if c = '"' then
+        State := STATE_QTEXT
+      else
+        break;
+    STATE_ATOM:
+      if c = '@' then
+        State := STATE_EXPECTING_SUBDOMAIN
+      else if c = '.' then
+        State := STATE_LOCAL_PERIOD
+      else if not (c in atom_chars) then
+        break;
+    STATE_QTEXT:
+      if c = '\' then
+        State := STATE_QCHAR
+      else if c = '"' then
+        State := STATE_QUOTE
+      else if not (c in quoted_string_chars) then
+        break;
+    STATE_QCHAR:
+      State := STATE_QTEXT;
+    STATE_QUOTE:
+      if c = '@' then
+        State := STATE_EXPECTING_SUBDOMAIN
+      else if c = '.' then
+        State := STATE_LOCAL_PERIOD
+      else
+        break;
+    STATE_LOCAL_PERIOD:
+      if c in atom_chars then
+        State := STATE_ATOM
+      else if c = '"' then
+        State := STATE_QTEXT
+      else
+        break;
+    STATE_EXPECTING_SUBDOMAIN:
+      if c in letters then
+        State := STATE_SUBDOMAIN
+      else
+        break;
+    STATE_SUBDOMAIN:
+      if c = '.' then begin
+        inc(subdomains);
+        State := STATE_EXPECTING_SUBDOMAIN
+      end else if c = '-' then
+        State := STATE_HYPHEN
+      else if not (c in letters_digits) then
+        break;
+    STATE_HYPHEN:
+      if c in letters_digits then
+        State := STATE_SUBDOMAIN
+      else if c <> '-' then
+        break;
+    end;
+    inc(i);
+  end;
+  if i <= n then
+    Result := False
+  else
+    Result := (State = STATE_SUBDOMAIN) and (subdomains >= 2);
+end;
 
 initialization
   Randomize;
