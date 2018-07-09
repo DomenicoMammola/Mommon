56096e50b242e69ba9e79c9d1a112a9979786bbe
Domenico Mammola <mimmo71@gmail.com>
2018-07-09 18:25:14 +0200

    * initial support for shared transaction between data managers (limited to db data managers at the moment)

--- src/database/mDatabaseConnection.pas
+++ src/database/mDatabaseConnection.pas
@@ -19,30 +19,67 @@ interface
 
 uses
   Classes, DB,
-  mDatabaseConnectionClasses, mDatabaseConnectionImpl;
+  mDatabaseConnectionClasses, mDatabaseConnectionImpl, mDataManagerClasses;
 
 type
   { TmDatabaseConnection }
 
   TmDatabaseConnection = class
-    private
-      FConnectionInfo : TmDatabaseConnectionInfo;
+  private
+    FConnectionInfo : TmDatabaseConnectionInfo;
 
-      FImplementation : TmDatabaseConnectionImpl;
-      procedure CreateImplementation;
-    public
-      constructor Create; overload;
-      constructor Create(const aConnectionInfo : TmDatabaseConnectionInfo); overload;
-      destructor Destroy; override;
+    FImplementation : TmDatabaseConnectionImpl;
+    procedure CreateImplementation;
+  public
+    constructor Create; overload;
+    constructor Create(const aConnectionInfo : TmDatabaseConnectionInfo); overload;
+    destructor Destroy; override;
 
-      procedure Connect;
-      procedure Close;
+    procedure Connect;
+    procedure Close;
 
-      procedure StartTransaction;
-      procedure Commit;
-      procedure Rollback;
+    procedure StartTransaction;
+    procedure Commit;
+    procedure Rollback;
 
-      property ConnectionInfo : TmDatabaseConnectionInfo read FConnectionInfo write FConnectionInfo;
+    property ConnectionInfo : TmDatabaseConnectionInfo read FConnectionInfo write FConnectionInfo;
+  end;
+
+  { TmDatabaseTransaction }
+
+  TmDatabaseTransaction = class(TmDataManagerTransaction)
+  strict private
+    FDatabaseConnection : TmDatabaseConnection;
+  public
+    constructor Create;
+
+    property DatabaseConnection : TmDatabaseConnection read FDatabaseConnection write FDatabaseConnection;
+  end;
+
+  { TmDatabaseConnectionsTandem }
+
+  TmDatabaseConnectionsTandem = class
+  strict private
+    FOwnedConnection : TmDatabaseConnection;
+    FSharedConnection : TmDatabaseConnection;
+    FConnectionInfo : TmDatabaseConnectionInfo;
+    FDatabaseTransaction : TmDatabaseTransaction;
+    procedure SetSharedConnection (aConnection : TmDatabaseConnection);
+    procedure CreateOwnedConnectionIfNeeded;
+  public
+    constructor Create(const aConnectionInfo : TmDatabaseConnectionInfo; aSharedTransaction : TmDatabaseTransaction);
+
+    destructor Destroy; override;
+
+    procedure ConnectIfOwned;
+    procedure CloseIfOwned;
+
+    procedure StartTransactionIfOwned;
+    procedure CommitIfOwned;
+    procedure RollbackIfOwned;
+
+    function Connection : TmDatabaseConnection;
+    function Transaction : TmDatabaseTransaction;
   end;
 
   { TmAbstractDatabaseCommand }
@@ -127,264 +164,377 @@ begin
   Result := _LastSQLScript;
 end;
 
-  { TmAbstractDatabaseCommand }
+{ TmDatabaseTransaction }
 
-  function TmAbstractDatabaseCommand.PrepareIfNecessary: Boolean;
+constructor TmDatabaseTransaction.Create;
+begin
+  FDatabaseConnection := nil;
+end;
+
+{ TmDatabaseConnectionsTandem }
+
+constructor TmDatabaseConnectionsTandem.Create(const aConnectionInfo: TmDatabaseConnectionInfo; aSharedTransaction : TmDatabaseTransaction);
+begin
+  FOwnedConnection := nil;
+  FSharedConnection := nil;
+  FDatabaseTransaction := nil;
+  FConnectionInfo := aConnectionInfo;
+  if Assigned(aSharedTransaction) and Assigned(aSharedTransaction.DatabaseConnection) then
+    Self.SetSharedConnection(aSharedTransaction.DatabaseConnection);
+end;
+
+procedure TmDatabaseConnectionsTandem.SetSharedConnection(aConnection: TmDatabaseConnection);
+begin
+  if Assigned(aConnection) then
   begin
-    Result := false;
-    if (not FImplementation.Prepared) then
+    if Assigned(FOwnedConnection) then
+      raise Exception.Create('Owned connection already created!');
+    FSharedConnection := aConnection;
+  end;
+end;
+
+procedure TmDatabaseConnectionsTandem.CreateOwnedConnectionIfNeeded;
+begin
+  if not Assigned(FOwnedConnection) then
+  begin
+    FOwnedConnection := TmDatabaseConnection.Create;
+    if Assigned(FConnectionInfo) then
+      FOwnedConnection.ConnectionInfo := FConnectionInfo;
+  end;
+end;
+
+destructor TmDatabaseConnectionsTandem.Destroy;
+begin
+  FreeAndNil(FOwnedConnection);
+  FreeAndNil(FDatabaseTransaction);
+  inherited Destroy;
+end;
+
+procedure TmDatabaseConnectionsTandem.ConnectIfOwned;
+begin
+  if not Assigned(FSharedConnection) then
+  begin
+    CreateOwnedConnectionIfNeeded;
+    FOwnedConnection.Connect;
+  end;
+end;
+
+procedure TmDatabaseConnectionsTandem.CloseIfOwned;
+begin
+  if not Assigned(FSharedConnection) then
+  begin
+    CreateOwnedConnectionIfNeeded;
+    FOwnedConnection.Close;
+  end;
+end;
+
+procedure TmDatabaseConnectionsTandem.StartTransactionIfOwned;
+begin
+  if not Assigned(FSharedConnection) then
+  begin
+    CreateOwnedConnectionIfNeeded;
+    FOwnedConnection.StartTransaction;
+  end;
+end;
+
+procedure TmDatabaseConnectionsTandem.CommitIfOwned;
+begin
+  if not Assigned(FSharedConnection) then
+  begin
+    CreateOwnedConnectionIfNeeded;
+    FOwnedConnection.Commit;
+  end;
+end;
+
+procedure TmDatabaseConnectionsTandem.RollbackIfOwned;
+begin
+  if not Assigned(FSharedConnection) then
+  begin
+    CreateOwnedConnectionIfNeeded;
+    FOwnedConnection.Rollback;
+  end;
+end;
+
+function TmDatabaseConnectionsTandem.Connection: TmDatabaseConnection;
+begin
+  Result := nil;
+  if Assigned(FSharedConnection) then
+    Result := FSharedConnection
+  else
+  begin
+    CreateOwnedConnectionIfNeeded;
+    Result := FOwnedConnection;
+  end;
+end;
+
+function TmDatabaseConnectionsTandem.Transaction: TmDatabaseTransaction;
+begin
+  if not Assigned(FDatabaseTransaction) then
+  begin
+    FDatabaseTransaction := TmDatabaseTransaction.Create;
+    FDatabaseTransaction.DatabaseConnection := Self.Connection;
+  end;
+  Result := FDatabaseTransaction;
+end;
+
+{ TmAbstractDatabaseCommand }
+
+function TmAbstractDatabaseCommand.PrepareIfNecessary: Boolean;
+begin
+  Result := false;
+  if (not FImplementation.Prepared) then
+  begin
+    Self.Prepare;
+    Result := true;
+  end
+  else
+  begin
+    if (not FImplementation.SameSQL(FSQL)) then
     begin
+      Self.Unprepare;
       Self.Prepare;
       Result := true;
-    end
-    else
-    begin
-      if (not FImplementation.SameSQL(FSQL)) then
-      begin
-        Self.Unprepare;
-        Self.Prepare;
-        Result := true;
-      end;
     end;
   end;
+end;
 
-  procedure TmAbstractDatabaseCommand.ReloadParameters;
-  var
-    i : integer;
+procedure TmAbstractDatabaseCommand.ReloadParameters;
+var
+  i : integer;
+begin
+  FParameters.Clear;
+  for i := 0 to FImplementation.ParamCount - 1 do
   begin
-    FParameters.Clear;
-    for i := 0 to FImplementation.ParamCount - 1 do
-    begin
-      FParameters.Add(FImplementation.GetParam(i));
-    end;
+    FParameters.Add(FImplementation.GetParam(i));
   end;
+end;
 
-  constructor TmAbstractDatabaseCommand.Create;
-  begin
-    FSQL := TStringList.Create;
-    FParameters := TmQueryParameters.Create;
-  end;
+constructor TmAbstractDatabaseCommand.Create;
+begin
+  FSQL := TStringList.Create;
+  FParameters := TmQueryParameters.Create;
+end;
 
-  destructor TmAbstractDatabaseCommand.Destroy;
-  begin
-    FreeAndNil(FSQL);
-    FreeAndNil(FParameters);
-    inherited Destroy;
-  end;
+destructor TmAbstractDatabaseCommand.Destroy;
+begin
+  FreeAndNil(FSQL);
+  FreeAndNil(FParameters);
+  inherited Destroy;
+end;
 
-  procedure TmAbstractDatabaseCommand.Prepare;
+procedure TmAbstractDatabaseCommand.Prepare;
+begin
+  CreateImplementation;
+  if FImplementation.Prepared then
+    FImplementation.Unprepare;
+  FImplementation.SetSQL(FSQL);
+  FImplementation.Prepare;
+  ReloadParameters;
+end;
+
+procedure TmAbstractDatabaseCommand.Unprepare;
+begin
+  CreateImplementation;
+  FImplementation.Unprepare;
+  FParameters.Clear;
+end;
+
+function TmAbstractDatabaseCommand.Prepared: boolean;
+begin
+  CreateImplementation;
+  Result := FImplementation.Prepared;
+end;
+
+function TmAbstractDatabaseCommand.ParamByName(const Value: string): TmQueryParameter;
+begin
+  CreateImplementation;
+  if PrepareIfNecessary then
   begin
-    CreateImplementation;
-    if FImplementation.Prepared then
-      FImplementation.Unprepare;
-    FImplementation.SetSQL(FSQL);
-    FImplementation.Prepare;
+    // must reload FParameters
     ReloadParameters;
   end;
+  Result := FParameters.FindByName(Value);
+end;
 
-  procedure TmAbstractDatabaseCommand.Unprepare;
+{ TmDatabaseCommand }
+
+procedure TmDatabaseCommand.CreateImplementation;
+begin
+  if not Assigned(FImplementation) then
   begin
-    CreateImplementation;
-    FImplementation.Unprepare;
-    FParameters.Clear;
+    if not Assigned(FDatabaseConnection) then
+      raise Exception.Create('No database connection was associated to the TmDatabaseCommand');
+    FImplementation := GetDataConnectionClassesRegister.GetCommandImpl(FDatabaseConnection.FImplementation.GetName);
+    FImplementation.DatabaseConnectionImpl := FDatabaseConnection.FImplementation;
   end;
+end;
 
-  function TmAbstractDatabaseCommand.Prepared: boolean;
+constructor TmDatabaseCommand.Create;
+begin
+  inherited;
+  FImplementation := nil;
+end;
+
+destructor TmDatabaseCommand.Destroy;
+begin
+  if Assigned(FImplementation) then
+    FreeAndNil(FImplementation);
+  inherited Destroy;
+end;
+
+function TmDatabaseCommand.Execute: integer;
+var
+  i : integer;
+begin
+  TraceSQL(SQL.Text);
+  CreateImplementation;
+  PrepareIfNecessary;
+  for i := 0 to FParameters.Count -1 do
   begin
-    CreateImplementation;
-    Result := FImplementation.Prepared;
+    FImplementation.SetParamValue(FParameters.GetParam(i));
   end;
+  Result := (FImplementation as TmDatabaseCommandImpl).Execute;
+end;
 
-  function TmAbstractDatabaseCommand.ParamByName(const Value: string): TmQueryParameter;
+
+{ TmDatabaseQuery }
+
+procedure TmDatabaseQuery.CreateImplementation;
+begin
+  if not Assigned(FImplementation) then
   begin
-    CreateImplementation;
-    if PrepareIfNecessary then
-    begin
-      // must reload FParameters
-      ReloadParameters;
-    end;
-    Result := FParameters.FindByName(Value);
+    FImplementation := GetDataConnectionClassesRegister.GetQueryImpl(FDatabaseConnection.FImplementation.GetName);
+    FImplementation.DatabaseConnectionImpl := FDatabaseConnection.FImplementation;
   end;
+end;
 
-  { TmDatabaseCommand }
+constructor TmDatabaseQuery.Create;
+begin
+  inherited;
+  FImplementation := nil;
+end;
 
-  procedure TmDatabaseCommand.CreateImplementation;
+destructor TmDatabaseQuery.Destroy;
+begin
+  if Assigned(FImplementation) then
+    FreeAndNil(FImplementation);
+  inherited Destroy;
+end;
+
+procedure TmDatabaseQuery.Open;
+var
+  i : integer;
+begin
+  TraceSQL(SQL.Text);
+  CreateImplementation;
+  PrepareIfNecessary;
+  for i := 0 to FParameters.Count -1 do
+  begin
+    FImplementation.SetParamValue(FParameters.GetParam(i));
+  end;
+  (FImplementation as TmDatabaseQueryImpl).Open;
+end;
+
+procedure TmDatabaseQuery.Close;
+begin
+  CreateImplementation;
+  (FImplementation as TmDatabaseQueryImpl).Close;
+end;
+
+procedure TmDatabaseQuery.First;
+begin
+  CreateImplementation;
+  (FImplementation as TmDatabaseQueryImpl).First;
+end;
+
+procedure TmDatabaseQuery.Next;
+begin
+  CreateImplementation;
+  (FImplementation as TmDatabaseQueryImpl).Next;
+end;
+
+function TmDatabaseQuery.Eof: boolean;
+begin
+  CreateImplementation;
+  Result := (FImplementation as TmDatabaseQueryImpl).Eof;
+end;
+
+function TmDatabaseQuery.AsDataset: TDataset;
+begin
+  CreateImplementation;
+  Result := (FImplementation as TmDatabaseQueryImpl).AsDataset;
+end;
+
+
+
+
+{ TmDatabaseConnection }
+
+procedure TmDatabaseConnection.CreateImplementation;
+begin
+  if Assigned(FConnectionInfo) then
   begin
     if not Assigned(FImplementation) then
     begin
-      if not Assigned(FDatabaseConnection) then
-        raise Exception.Create('No database connection was associated to the TmDatabaseCommand');
-      FImplementation := GetDataConnectionClassesRegister.GetCommandImpl(FDatabaseConnection.FImplementation.GetName);
-      FImplementation.DatabaseConnectionImpl := FDatabaseConnection.FImplementation;
-    end;
-  end;
-
-  constructor TmDatabaseCommand.Create;
-  begin
-    inherited;
-    FImplementation := nil;
-  end;
-
-  destructor TmDatabaseCommand.Destroy;
-  begin
-    if Assigned(FImplementation) then
-      FreeAndNil(FImplementation);
-    inherited Destroy;
-  end;
-
-  function TmDatabaseCommand.Execute: integer;
-  var
-    i : integer;
-  begin
-    TraceSQL(SQL.Text);
-    CreateImplementation;
-    PrepareIfNecessary;
-    for i := 0 to FParameters.Count -1 do
-    begin
-      FImplementation.SetParamValue(FParameters.GetParam(i));
-    end;
-    Result := (FImplementation as TmDatabaseCommandImpl).Execute;
-  end;
-
-
-  { TmDatabaseQuery }
-
-  procedure TmDatabaseQuery.CreateImplementation;
-  begin
-    if not Assigned(FImplementation) then
-    begin
-      FImplementation := GetDataConnectionClassesRegister.GetQueryImpl(FDatabaseConnection.FImplementation.GetName);
-      FImplementation.DatabaseConnectionImpl := FDatabaseConnection.FImplementation;
-    end;
-  end;
-
-  constructor TmDatabaseQuery.Create;
-  begin
-    inherited;
-    FImplementation := nil;
-  end;
-
-  destructor TmDatabaseQuery.Destroy;
-  begin
-    if Assigned(FImplementation) then
-      FreeAndNil(FImplementation);
-    inherited Destroy;
-  end;
-
-  procedure TmDatabaseQuery.Open;
-  var
-    i : integer;
-  begin
-    TraceSQL(SQL.Text);
-    CreateImplementation;
-    PrepareIfNecessary;
-    for i := 0 to FParameters.Count -1 do
-    begin
-      FImplementation.SetParamValue(FParameters.GetParam(i));
-    end;
-    (FImplementation as TmDatabaseQueryImpl).Open;
-  end;
-
-  procedure TmDatabaseQuery.Close;
-  begin
-    CreateImplementation;
-    (FImplementation as TmDatabaseQueryImpl).Close;
-  end;
-
-  procedure TmDatabaseQuery.First;
-  begin
-    CreateImplementation;
-    (FImplementation as TmDatabaseQueryImpl).First;
-  end;
-
-  procedure TmDatabaseQuery.Next;
-  begin
-    CreateImplementation;
-    (FImplementation as TmDatabaseQueryImpl).Next;
-  end;
-
-  function TmDatabaseQuery.Eof: boolean;
-  begin
-    CreateImplementation;
-    Result := (FImplementation as TmDatabaseQueryImpl).Eof;
-  end;
-
-  function TmDatabaseQuery.AsDataset: TDataset;
-  begin
-    CreateImplementation;
-    Result := (FImplementation as TmDatabaseQueryImpl).AsDataset;
-  end;
-
-
-
-
-  { TmDatabaseConnection }
-
-  procedure TmDatabaseConnection.CreateImplementation;
-  begin
-    if Assigned(FConnectionInfo) then
-    begin
+      FImplementation := GetDataConnectionClassesRegister.GetConnectionImpl(FConnectionInfo.VendorType);
       if not Assigned(FImplementation) then
-      begin
-        FImplementation := GetDataConnectionClassesRegister.GetConnectionImpl(FConnectionInfo.VendorType);
-        if not Assigned(FImplementation) then
-          raise TmDataConnectionException.Create('No connection implementation available for ' + DatabaseVendorToString(FConnectionInfo.VendorType) + '.');
-        FImplementation.ConnectionInfo := FConnectionInfo;
-      end;
-    end
-    else
-    begin
-      raise TmDataConnectionException.Create('Connection info is unavailable.');
+        raise TmDataConnectionException.Create('No connection implementation available for ' + DatabaseVendorToString(FConnectionInfo.VendorType) + '.');
+      FImplementation.ConnectionInfo := FConnectionInfo;
     end;
-  end;
-
-  constructor TmDatabaseConnection.Create;
+  end
+  else
   begin
-    FImplementation := nil;
+    raise TmDataConnectionException.Create('Connection info is unavailable.');
   end;
+end;
 
-  constructor TmDatabaseConnection.Create(const aConnectionInfo: TmDatabaseConnectionInfo);
-  begin
-    Self.Create;
-    FConnectionInfo := aConnectionInfo;
-  end;
+constructor TmDatabaseConnection.Create;
+begin
+  FImplementation := nil;
+end;
 
-  destructor TmDatabaseConnection.Destroy;
-  begin
-    if Assigned(FImplementation) then
-      FreeAndNil(FImplementation);
-    inherited Destroy;
-  end;
+constructor TmDatabaseConnection.Create(const aConnectionInfo: TmDatabaseConnectionInfo);
+begin
+  Self.Create;
+  FConnectionInfo := aConnectionInfo;
+end;
 
-  procedure TmDatabaseConnection.Connect;
-  begin
-    CreateImplementation;
-    FImplementation.Connect;
-  end;
+destructor TmDatabaseConnection.Destroy;
+begin
+  if Assigned(FImplementation) then
+    FreeAndNil(FImplementation);
+  inherited Destroy;
+end;
 
-  procedure TmDatabaseConnection.Close;
-  begin
-    CreateImplementation;
-    FImplementation.Close;
-  end;
+procedure TmDatabaseConnection.Connect;
+begin
+  CreateImplementation;
+  FImplementation.Connect;
+end;
 
-  procedure TmDatabaseConnection.StartTransaction;
-  begin
-    CreateImplementation;
-    FImplementation.StartTransaction;
-  end;
+procedure TmDatabaseConnection.Close;
+begin
+  CreateImplementation;
+  FImplementation.Close;
+end;
 
-  procedure TmDatabaseConnection.Commit;
-  begin
-    CreateImplementation;
-    FImplementation.Commit;
-  end;
+procedure TmDatabaseConnection.StartTransaction;
+begin
+  CreateImplementation;
+  FImplementation.StartTransaction;
+end;
 
-  procedure TmDatabaseConnection.Rollback;
-  begin
-    CreateImplementation;
-    FImplementation.Rollback;
-  end;
+procedure TmDatabaseConnection.Commit;
+begin
+  CreateImplementation;
+  FImplementation.Commit;
+end;
+
+procedure TmDatabaseConnection.Rollback;
+begin
+  CreateImplementation;
+  FImplementation.Rollback;
+end;
 
 initialization
   _LastSQLScript := '';
