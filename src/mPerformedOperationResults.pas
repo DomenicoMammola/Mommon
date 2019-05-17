c300824944974746ac6e3f1e99a5990a6984c444
Domenico Mammola <mimmo71@gmail.com>
2019-05-17 17:56:46 +0200

    * add the ability to force the user to check some particular log messages (how to perform validation is in charge of the display form)

--- src/mPerformedOperationResults.pas
+++ src/mPerformedOperationResults.pas
@@ -18,27 +18,58 @@ unit mPerformedOperationResults;
 interface
 
 uses
-  Classes;
+  Classes, contnrs;
 
 type
   IPerformedOperationResults = interface
     ['{1AFCFE0A-2F11-4A92-8396-C3F70B5181F2}']
-    procedure AddError (const aMessage: String);
-    procedure AddWarning (const aMessage: String);
-    procedure AddInfo (const aMessage: String);
-    procedure AddResult (const aMessage: String);
+    procedure AddError (const aMessage: String; const aMustBeValidated : boolean = false);
+    procedure AddWarning (const aMessage: String; const aMustBeValidated : boolean = false);
+    procedure AddInfo (const aMessage: String; const aMustBeValidated : boolean = false);
+    procedure AddResult (const aMessage: String; const aMustBeValidated : boolean = false);
     procedure GetMessagesAsStrings(aResults : TStringList);
   end;
 
+  { TPerformedOperation }
 
+  TPerformedOperation = class
+  strict private
+    FMessage : String;
+    FMustBeValidated : boolean;
+  public
+    constructor Create(const aMessage : String; const aMustBeValidated: boolean); overload;
+    constructor Create(); overload;
+    destructor Destroy; override;
+
+    property Message : String read FMessage write FMessage;
+    property MustBeValidated : boolean read FMustBeValidated write FMustBeValidated;
+  end;
+
+  { TPerformedOperations }
+
+  TPerformedOperations = class
+  strict private
+    FList : TObjectList;
+  public
+    constructor Create();
+    destructor Destroy; override;
+    function Count : integer;
+    function Get(const aIndex : integer): TPerformedOperation;
+    function Add : TPerformedOperation; overload;
+    procedure Add(aItem : TPerformedOperation); overload;
+    procedure Clear;
+  end;
 
   { TPerformedOperationResultsAsLog }
 
   TPerformedOperationResultsAsLog = class (IPerformedOperationResults)
   strict private
-    FList : TStringList;
-    FWarnings : Word;
-    FErrors : Word;
+    FErrors : TPerformedOperations;
+    FWarnings : TPerformedOperations;
+    FInfos : TPerformedOperations;
+    FResults : TPerformedOperations;
+    FMessages : TStringList;
+
     function GetMessages: TStrings;
   public
     constructor Create;
@@ -46,72 +77,141 @@ type
 
     procedure Clear;
 
-    procedure AddError (const aMessage: String);
-    procedure AddWarning (const aMessage: String);
-    procedure AddInfo (const aMessage: String);
-    procedure AddResult (const aMessage: String);
+    procedure AddError (const aMessage: String; const aMustBeValidated : boolean = false);
+    procedure AddWarning (const aMessage: String; const aMustBeValidated : boolean = false);
+    procedure AddInfo (const aMessage: String; const aMustBeValidated : boolean = false);
+    procedure AddResult (const aMessage: String; const aMustBeValidated : boolean = false);
     procedure GetMessagesAsStrings(aMessages : TStringList);
 
     property Messages : TStrings read GetMessages;
-    property Warnings : Word read FWarnings;
-    property Errors : Word read FErrors;
+    property Warnings : TPerformedOperations read FWarnings;
+    property Errors : TPerformedOperations read FErrors;
+    property Infos : TPerformedOperations read FInfos;
+    property Results : TPerformedOperations read FResults;
   end;
 
 implementation
 
-{ TPerformedOperationResultsAsLog }
+{ TPerformedOperations }
 
-function TPerformedOperationResultsAsLog.GetMessages: TStrings;
+constructor TPerformedOperations.Create();
 begin
-  Result := FList;
+  FList := TObjectList.Create(true);
 end;
 
-constructor TPerformedOperationResultsAsLog.Create;
-begin
-  FList := TStringList.Create;
-  FWarnings:= 0;
-  FErrors:= 0;
-end;
-
-destructor TPerformedOperationResultsAsLog.Destroy;
+destructor TPerformedOperations.Destroy;
 begin
   FList.Free;
   inherited Destroy;
 end;
 
-procedure TPerformedOperationResultsAsLog.Clear;
+function TPerformedOperations.Count: integer;
+begin
+  Result := FList.Count;
+end;
+
+function TPerformedOperations.Get(const aIndex: integer): TPerformedOperation;
+begin
+  Result := FList.Items[aIndex] as TPerformedOperation;
+end;
+
+function TPerformedOperations.Add: TPerformedOperation;
+begin
+  Result := TPerformedOperation.Create();
+  Self.Add(Result);
+end;
+
+procedure TPerformedOperations.Add(aItem: TPerformedOperation);
+begin
+  FList.Add(aItem);
+end;
+
+procedure TPerformedOperations.Clear;
 begin
   FList.Clear;
-  FWarnings:= 0;
-  FErrors:= 0;
 end;
 
-procedure TPerformedOperationResultsAsLog.AddError(const aMessage: String);
+{ TPerformedOperation }
+
+constructor TPerformedOperation.Create(const aMessage : String; const aMustBeValidated: boolean);
 begin
-  FList.Append('[ERROR] ' + aMessage);
-  inc (FErrors);
+  FMessage:= aMessage;
+  FMustBeValidated:= aMustBeValidated;
 end;
 
-procedure TPerformedOperationResultsAsLog.AddWarning(const aMessage: String);
+constructor TPerformedOperation.Create();
 begin
-  FList.Append ('[WARNING] ' + aMessage);
-  inc (FWarnings);
+  Self.Create('', false);
 end;
 
-procedure TPerformedOperationResultsAsLog.AddInfo(const aMessage: String);
+destructor TPerformedOperation.Destroy;
 begin
-  FList.Append ('[INFO] ' + aMessage);
+  inherited Destroy;
 end;
 
-procedure TPerformedOperationResultsAsLog.AddResult(const aMessage: String);
+{ TPerformedOperationResultsAsLog }
+
+function TPerformedOperationResultsAsLog.GetMessages: TStrings;
 begin
-  FList.Append('[RESULT] ' + aMessage);
+  Result := FMessages;
+end;
+
+constructor TPerformedOperationResultsAsLog.Create;
+begin
+  FErrors := TPerformedOperations.Create();
+  FWarnings := TPerformedOperations.Create();
+  FInfos := TPerformedOperations.Create();
+  FResults := TPerformedOperations.Create();
+  FMessages := TStringList.Create;
+end;
+
+destructor TPerformedOperationResultsAsLog.Destroy;
+begin
+  FErrors.Free;
+  FWarnings.Free;
+  FInfos.Free;
+  FResults.Free;
+  FMessages.Free;
+  inherited Destroy;
+end;
+
+procedure TPerformedOperationResultsAsLog.Clear;
+begin
+  FErrors.Clear;
+  FWarnings.Clear;
+  FInfos.Clear;
+  FResults.Clear;
+  FMessages.Clear;
+end;
+
+procedure TPerformedOperationResultsAsLog.AddError(const aMessage: String; const aMustBeValidated : boolean = false);
+begin
+  FMessages.Append('[ERROR] ' + aMessage);
+  FErrors.Add(TPerformedOperation.Create(aMessage, aMustBeValidated));
+end;
+
+procedure TPerformedOperationResultsAsLog.AddWarning(const aMessage: String; const aMustBeValidated : boolean = false);
+begin
+  FMessages.Append ('[WARNING] ' + aMessage);
+  FWarnings.Add(TPerformedOperation.Create(aMessage, aMustBeValidated));
+end;
+
+procedure TPerformedOperationResultsAsLog.AddInfo(const aMessage: String; const aMustBeValidated : boolean = false);
+begin
+  FMessages.Append ('[INFO] ' + aMessage);
+  FInfos.Add(TPerformedOperation.Create(aMessage, aMustBeValidated));
+end;
+
+procedure TPerformedOperationResultsAsLog.AddResult(const aMessage: String; const aMustBeValidated : boolean = false);
+begin
+  FMessages.Append('[RESULT] ' + aMessage);
+  FResults.Add(TPerformedOperation.Create(aMessage, aMustBeValidated));
 end;
 
 procedure TPerformedOperationResultsAsLog.GetMessagesAsStrings(aMessages: TStringList);
 begin
   aMessages.Clear;
-  aMessages.AddStrings(FList);
+  aMessages.AddStrings(FMessages);
 end;
 
 end.
