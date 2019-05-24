0ad2aa0602b314947718e37de79970ad50d20ccc
Domenico Mammola <mimmo71@gmail.com>
2019-05-24 18:30:31 +0200

    * add WriteDate and WriteDateTime

--- src/mExcelUtils.pas
+++ src/mExcelUtils.pas
@@ -19,6 +19,12 @@ uses
 procedure WriteFloat(aSheet: TsWorksheet; const aRow, aCol : integer; const aValue : TNullableDouble; const aFractionalPartDigits : integer); overload;
 procedure WriteFloat(aSheet: TsWorksheet; const aRow, aCol : integer; const aValue : double; const aFractionalPartDigits : integer); overload;
 
+procedure WriteDate(aSheet: TsWorksheet; const aRow, aCol : integer; const aValue: TNullableDateTime); overload;
+procedure WriteDate(aSheet: TsWorksheet; const aRow, aCol : integer; const aValue : TDateTime); overload;
+
+procedure WriteDateTime(aSheet: TsWorksheet; const aRow, aCol : integer; const aValue: TNullableDateTime); overload;
+procedure WriteDateTime(aSheet: TsWorksheet; const aRow, aCol : integer; const aValue : TDateTime); overload;
+
 procedure WriteInteger(aSheet: TsWorksheet; const aRow, aCol : integer; const aValue : TNullableInteger); overload;
 procedure WriteInteger(aSheet: TsWorksheet; const aRow, aCol : integer; const aValue : integer); overload;
 procedure WriteInt64(aSheet: TsWorksheet; const aRow, aCol : integer; const aValue : Int64); overload;
@@ -27,7 +33,8 @@ implementation
 
 procedure WriteFloat(aSheet: TsWorksheet; const aRow, aCol: integer; const aValue: TNullableDouble; const aFractionalPartDigits: integer);
 begin
-  WriteFloat(aSheet, aRow, aCol, aValue.AsFloat, aFractionalPartDigits);
+  if aValue.NotNull then
+    WriteFloat(aSheet, aRow, aCol, aValue.AsFloat, aFractionalPartDigits);
 end;
 
 procedure WriteFloat(aSheet: TsWorksheet; const aRow, aCol: integer; const aValue: double; const aFractionalPartDigits: integer);
@@ -35,9 +42,33 @@ begin
   aSheet.WriteNumber(aRow, aCol, aValue, nfFixed, aFractionalPartDigits);
 end;
 
+procedure WriteDate(aSheet: TsWorksheet; const aRow, aCol: integer; const aValue: TDateTime);
+begin
+  aSheet.WriteDateTime(aRow, aCol, aValue, nfShortDate);
+end;
+
+procedure WriteDate(aSheet: TsWorksheet; const aRow, aCol: integer; const aValue: TNullableDateTime);
+begin
+  if aValue.NotNull then
+    WriteDate(aSheet, aRow, aCol, aValue.Value);
+end;
+
+procedure WriteDateTime(aSheet: TsWorksheet; const aRow, aCol: integer; const aValue: TNullableDateTime);
+begin
+  if aValue.NotNull then
+    WriteDateTime(aSheet, aRow, aCol, aValue.Value);
+
+end;
+
+procedure WriteDateTime(aSheet: TsWorksheet; const aRow, aCol: integer; const aValue: TDateTime);
+begin
+  aSheet.WriteDateTime(aRow, aCol, aValue, nfShortDateTime);
+end;
+
 procedure WriteInteger(aSheet: TsWorksheet; const aRow, aCol: integer; const aValue: TNullableInteger);
 begin
-  WriteInteger(aSheet, aRow, aCol, aValue.AsInteger);
+  if aValue.NotNull then
+    WriteInteger(aSheet, aRow, aCol, aValue.AsInteger);
 end;
 
 procedure WriteInteger(aSheet: TsWorksheet; const aRow, aCol: integer; const aValue: integer);
