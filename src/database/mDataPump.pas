35b0649a070984a419ae87227eb7108f65914494
Domenico Mammola <mimmo71@gmail.com>
2019-07-25 15:47:44 +0200

    * a data pump implementation, to transfer data between databases

---
+++ src/database/mDataPump.pas
@@ -0,0 +1,350 @@
+// This is part of the Mommon Library
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// This software is distributed without any warranty.
+//
+// @author Domenico Mammola (mimmo71@gmail.com - www.mammola.net)
+
+unit mDataPump;
+
+{$IFDEF FPC}
+  {$MODE DELPHI}
+{$ENDIF}
+
+interface
+
+{$I mDefines.inc}
+
+uses
+  DB, Classes,
+  mDatabaseConnection, mDatabaseConnectionClasses,
+  mProgress, mThreads,
+  mDataPumpConfiguration;
+
+type
+
+  { TReplicaEngine }
+
+  TReplicaEngine = class
+  strict private
+    FCanTerminate : boolean;
+
+    procedure InternalReplicaTable (aProgress: ImProgress; aData : TObject; aJobResult : TJobResult);
+    procedure DoTerminate(const aJobsResult : TJobResults);
+  public
+    function ProcessTables (const aTables : TDataReplicaTables; const aMaxConcurrentThreads : integer = -1) : boolean;
+  end;
+
+  function DoReplicaTable (const aTable : TDataReplicaTableToTable) : boolean;
+
+
+implementation
+
+uses
+  SysUtils, md5, variants,
+  mSQLBuilder, mFilterOperators, mMaps, mUtility;
+
+type
+
+  { TParameterTypeShell }
+
+  TParameterTypeShell = class
+  public
+    ParamType : TmParameterDataType;
+
+    constructor Create(const aParameterType : TmParameterDataType);
+  end;
+
+function ComposeDestinationSelectQuery (aTable: TDataReplicaTableToTable; const aDestinationSB : TmSQLBuilder): String;
+var
+  i : integer;
+  comma, andStr : String;
+  curFieldToField: TDataReplicaFieldToField;
+begin
+  Result := 'select ';
+  comma := '';
+  for i := 0 to aTable.FieldsMapping.Count - 1 do
+  begin
+    Result := Result + comma + aDestinationSB.SQLDialectExpert.GetSQLForFieldname(aTable.FieldsMapping.Get(i).DestinationField.AsString);
+    comma := ','
+  end;
+  Result := Result + ' from ' + aDestinationSB.SQLDialectExpert.GetSQLForTablename(aTable.DestinationTableName);
+  Result := Result + ' where ';
+  andStr := '';
+  for i := 0 to aTable.SourceKeyFields.Count - 1 do
+  begin
+    curFieldToField := aTable.FieldsMapping.GetBySourceFieldName(aTable.SourceKeyFields.Strings[i]);
+    if not Assigned(curFieldToField) then
+      raise Exception.Create('Key field ' + aTable.SourceKeyFields.Strings[i] + ' not found in source query');
+    Result := Result + andStr + aDestinationSB.SQLSnippetForCondition(curFieldToField.DestinationField.AsString, foEq, aTable.SourceKeyFields.Strings[i]);
+    andStr := 'and';
+  end;
+end;
+
+function ComposeDestinationInsertQuery (aTable: TDataReplicaTableToTable; const aDestinationSB : TmSQLBuilder): String;
+var
+  i: integer;
+  comma: String;
+begin
+  Result := 'insert into ' + aDestinationSB.SQLDialectExpert.GetSQLForTablename(aTable.DestinationTableName) + ' (';
+  comma := '';
+  for i:= 0 to aTable.FieldsMapping.Count - 1 do
+  begin
+    Result := Result + comma + aDestinationSB.SQLDialectExpert.GetSQLForFieldname(aTable.FieldsMapping.Get(i).DestinationField.AsString);
+    comma := ','
+  end;
+  Result := Result + ') values (';
+  comma := '';
+  for i:= 0 to aTable.FieldsMapping.Count - 1 do
+  begin
+    Result := Result + comma + aDestinationSB.SQLSnippetForValue(aTable.FieldsMapping.Get(i).SourceField.AsString);
+    comma := ','
+  end;
+  Result := Result + ')';
+end;
+
+function ComposeDestinationUpdateQuery (aTable: TDataReplicaTableToTable; const aDestinationSB : TmSQLBuilder): String;
+var
+  i : integer;
+  comma, andStr: String;
+  curFieldToField: TDataReplicaFieldToField;
+begin
+  Result := 'update ' + aDestinationSB.SQLDialectExpert.GetSQLForTablename(aTable.DestinationTableName) + ' set ';
+  comma := '';
+  for i:= 0 to aTable.FieldsMapping.Count - 1 do
+  begin
+    if aTable.SourceKeyFields.IndexOf(aTable.FieldsMapping.Get(i).SourceField.AsString) < 0 then
+    begin
+      Result := Result + comma + aDestinationSB.SQLDialectExpert.GetSQLForFieldname(aTable.FieldsMapping.Get(i).DestinationField.AsString) + '=' + aDestinationSB.SQLSnippetForValue(aTable.FieldsMapping.Get(i).SourceField.AsString);
+      comma := ','
+    end;
+  end;
+  Result := Result + ' where ';
+  andStr := '';
+  for i := 0 to aTable.SourceKeyFields.Count - 1 do
+  begin
+    curFieldToField := aTable.FieldsMapping.GetBySourceFieldName(aTable.SourceKeyFields.Strings[i]);
+    Result := Result + andStr + aDestinationSB.SQLSnippetForCondition(curFieldToField.DestinationField.AsString, foEq, aTable.SourceKeyFields.Strings[i]);
+    andStr := 'and';
+  end;
+end;
+
+function DoReplicaTable(const aTable : TDataReplicaTableToTable): boolean;
+var
+  q: integer;
+  sourceConnection, destinationConnection: TmDatabaseConnection;
+  sourceQuery, destinationQuery: TmDatabaseQuery;
+  SQLBuilderDestination, SQLBuilderInsert, SQLBuilderUpdate: TmSQLBuilder;
+  performInsert, performUpdate: boolean;
+  command : TmDatabaseCommand;
+  sourcefld, destinationfld : TField;
+  destinationFieldsMap, sourceFieldsMap : TmStringDictionary;
+  tmpsql: String;
+  tmpparameter : TmQueryParameter;
+  paramshell : TParameterTypeShell;
+begin
+  Result := false;
+  sourceConnection := TmDatabaseConnection.Create(aTable.SourceConnectionInfo);
+  destinationConnection := TmDatabaseConnection.Create(aTable.DestinationConnectionInfo);
+  try
+    sourceConnection.Connect;
+    destinationConnection.Connect;
+
+    destinationConnection.StartTransaction;
+    try
+      sourceQuery := TmDatabaseQuery.Create;
+      command := TmDatabaseCommand.Create;
+      destinationQuery := TmDatabaseQuery.Create;
+      SQLBuilderDestination:=  TmSQLBuilder.Create;
+      SQLBuilderInsert := TmSQLBuilder.Create;
+      SQLBuilderUpdate := TmSQLBuilder.Create;
+      destinationFieldsMap := TmStringDictionary.Create(true);
+      sourceFieldsMap := TmStringDictionary.Create(false);
+      try
+        sourceQuery.DatabaseConnection:= sourceConnection;
+        destinationQuery.DatabaseConnection:= destinationConnection;
+        command.DatabaseConnection:= destinationConnection;
+        SQLBuilderDestination.VendorType:= destinationConnection.ConnectionInfo.VendorType;
+        SQLBuilderInsert.VendorType:= SQLBuilderDestination.VendorType;
+        SQLBuilderUpdate.VendorType:= SQLBuilderDestination.VendorType;
+
+        aTable.FieldsMapping.RebuildIndexes;
+
+        sourceQuery.SetSQL(aTable.SourceSelectQuery);
+        SQLBuilderDestination.PrepareSQL(ComposeDestinationSelectQuery(aTable, SQLBuilderDestination));
+        SQLBuilderInsert.PrepareSQL(ComposeDestinationInsertQuery(aTable, SQLBuilderInsert));
+        SQLBuilderUpdate.PrepareSQL(ComposeDestinationUpdateQuery(aTable, SQLBuilderUpdate));
+
+        sourceFieldsMap.Clear;
+        sourceQuery.Open;
+        while not sourceQuery.Eof do
+        begin
+          if sourceFieldsMap.Count = 0 then
+          begin
+            for q := 0 to sourceQuery.AsDataset.Fields.Count - 1 do
+              sourceFieldsMap.Add(Uppercase(sourceQuery.AsDataset.Fields[q].FieldName), sourceQuery.AsDataset.Fields[q]);
+          end;
+
+          for q := 0 to aTable.SourceKeyFields.Count - 1 do
+          begin
+            sourcefld := sourceFieldsMap.Find(Uppercase(aTable.SourceKeyFields.Strings[q])) as TField;
+            if not Assigned(sourcefld) then
+              raise Exception.Create('Field ' + aTable.SourceKeyFields.Strings[q] + ' not found in source query');
+            SQLBuilderDestination.ParamByName(aTable.SourceKeyFields.Strings[q]).Assign(sourcefld);
+          end;
+
+          tmpsql := SQLBuilderDestination.BuildSQL;
+          destinationQuery.SetSQL(tmpsql);
+          destinationQuery.Open;
+          performInsert := destinationQuery.Eof;
+          performUpdate := false;
+          if destinationFieldsMap.Count = 0 then
+          begin
+            for q := 0 to destinationQuery.AsDataset.Fields.Count - 1 do
+            begin
+              destinationFieldsMap.Add(Uppercase(destinationQuery.AsDataset.Fields[q].FieldName), TParameterTypeShell.Create(DataTypeToParameterDataType(destinationQuery.AsDataset.Fields[q].DataType)));
+            end;
+          end;
+          if not performInsert then
+          begin
+            for q := 0 to aTable.FieldsMapping.Count - 1 do
+            begin
+              sourcefld := sourceFieldsMap.Find(aTable.FieldsMapping.Get(q).SourceField.AsUppercaseString) as TField;
+              destinationfld := destinationQuery.AsDataset.FieldByName(aTable.FieldsMapping.Get(q).DestinationField.AsString);
+              performUpdate := MD5Print(MD5String(sourcefld.AsString)) <> MD5Print(MD5String(destinationfld.AsString));
+              if performUpdate then
+                break;
+            end;
+          end;
+          destinationQuery.Close;
+
+          if performInsert then
+          begin
+            for q := 0 to aTable.FieldsMapping.Count - 1 do
+            begin
+              sourcefld := sourceFieldsMap.Find(aTable.FieldsMapping.Get (q).SourceField.AsUppercaseString) as TField;
+              if not Assigned(sourcefld) then
+                raise Exception.Create('Field ' + aTable.FieldsMapping.Get (q).SourceField.AsString + ' not found as source field in configuration');
+              tmpparameter := SQLBuilderInsert.ParamByName(sourcefld.FieldName);
+              if not Assigned(tmpparameter) then
+                raise Exception.Create('Parameter ' + sourcefld.FieldName + ' not found in insert query');
+
+              paramshell := destinationFieldsMap.Find(aTable.FieldsMapping.Get(q).DestinationField.AsUppercaseString) as TParameterTypeShell;
+              if not Assigned(paramshell) then
+                raise Exception.Create('Field ' + aTable.FieldsMapping.Get(q).DestinationField.AsString + ' not found in destination table');
+              tmpparameter.Assign(sourcefld.AsVariant, paramshell.ParamType);
+            end;
+            tmpsql:= SQLBuilderInsert.BuildSQL;
+            command.SetSQL(tmpsql);
+            //writeln(tmpSql);
+            command.Execute;
+          end
+          else
+          if performUpdate then begin
+            for q := 0 to aTable.FieldsMapping.Count - 1 do
+            begin
+              sourcefld := sourceFieldsMap.Find(aTable.FieldsMapping.Get (q).SourceField.AsUppercaseString) as TField;
+              SQLBuilderUpdate.ParamByName(sourcefld.FieldName).Assign(sourcefld.AsVariant, (destinationFieldsMap.Find(aTable.FieldsMapping.Get(q).DestinationField.AsUppercaseString) as TParameterTypeShell).ParamType);
+            end;
+            tmpsql:= SQLBuilderUpdate.BuildSQL;
+            //writeln(tmpSQL);
+            command.SetSQL(tmpsql);
+            command.Execute;
+          end;
+
+          sourceQuery.Next;
+        end;
+      finally
+        sourceFieldsMap.Free;
+        destinationFieldsMap.Free;
+        SQLBuilderDestination.Free;
+        SQLBuilderInsert.Free;
+        SQLBuilderUpdate.Free;
+        destinationQuery.Free;
+        sourceQuery.Free;
+        command.Free;
+      end;
+
+      destinationConnection.Commit;
+    except
+      on e : Exception do
+      begin
+        destinationConnection.Rollback;
+        raise;
+      end;
+    end;
+
+    sourceConnection.Close;
+    destinationConnection.Close;
+  finally
+    destinationConnection.Free;
+    sourceConnection.Free;
+  end;
+
+  Result := true;
+end;
+
+{ TReplicaEngine }
+
+procedure TReplicaEngine.InternalReplicaTable(aProgress: ImProgress; aData : TObject; aJobResult: TJobResult);
+begin
+  if DoReplicaTable((aData as TDataReplicaTableToTable)) then
+    aJobResult.ReturnCode:= 1
+  else
+    aJobResult.ReturnCode:= -1;
+end;
+
+procedure TReplicaEngine.DoTerminate(const aJobsResult: TJobResults);
+begin
+  FCanTerminate:= true;
+end;
+
+function TReplicaEngine.ProcessTables(const aTables: TDataReplicaTables; const aMaxConcurrentThreads: integer): boolean;
+var
+  batchexec : TmBatchExecutor;
+  newjob : TmJob;
+  i : integer;
+begin
+  Result := false;
+  if aTables.Count = 0 then
+    exit;
+  batchexec := TmBatchExecutor.Create;
+  try
+    if aMaxConcurrentThreads > 0 then
+      batchexec.MaxConcurrentThreads:= aMaxConcurrentThreads
+    else
+      batchexec.MaxConcurrentThreads:= GetCPUCores;
+
+    for i := 0 to aTables.Count - 1 do
+    begin
+      newjob := batchexec.QueueJob;
+      newjob.Description:= aTables.Get(i).DestinationTableName;
+      newjob.DoJobProcedure := InternalReplicaTable;
+      newJob.Data := aTables.Get(i);
+    end;
+
+    FCanTerminate:= false;
+    batchexec.Execute(DoTerminate);
+    while not FCanTerminate do
+    begin
+      sleep(100);
+      {$IFDEF NOGUI}
+      CheckSynchronize;
+      {$ENDIF}
+    end;
+  finally
+    batchexec.Free;
+  end;
+end;
+
+{ TParameterTypeShell }
+
+constructor TParameterTypeShell.Create(const aParameterType: TmParameterDataType);
+begin
+  ParamType:= aParameterType;
+end;
+
+end.
---
+++ src/database/mDataPumpConfiguration.pas
@@ -0,0 +1,215 @@
+// This is part of the Mommon Library
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// This software is distributed without any warranty.
+//
+// @author Domenico Mammola (mimmo71@gmail.com - www.mammola.net)
+
+unit mDataPumpConfiguration;
+
+{$IFDEF FPC}
+  {$MODE DELPHI}
+{$ENDIF}
+
+interface
+
+uses
+  Classes, contnrs,
+  mNullables, mMaps, mDatabaseConnectionClasses;
+
+type
+
+  { TDataReplicaFieldToField }
+
+  TDataReplicaFieldToField = class
+  strict private
+    FSourceField : TNullableString;
+    FDestinationField : TNullableString;
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    property SourceField : TNullableString read FSourceField;
+    property DestinationField : TNullableString read FDestinationField;
+  end;
+
+  { TDataReplicaFields }
+
+  TDataReplicaFields = class
+  strict private
+    FList : TObjectList;
+    FIndexBySourceFieldName : TmStringDictionary;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    procedure RebuildIndexes;
+
+    function Count : integer;
+    function Get(const aIndex : integer) : TDataReplicaFieldToField;
+    function Add: TDataReplicaFieldToField;
+    function GetBySourceFieldName (const aSourceFieldName : String): TDataReplicaFieldToField;
+  end;
+
+
+  { TDataReplicaTableToTable }
+
+  TDataReplicaTableToTable = class
+  strict private
+    FSourceSelectQuery : String;
+    FDestinationSelectQuery : String;
+
+    FDestinationTableName : String;
+    FSourceKeyFields : TStringList;
+    FFieldsMapping : TDataReplicaFields;
+
+    FSourceConnectionInfo: TmDatabaseConnectionInfo;
+    FDestinationConnectionInfo : TmDatabaseConnectionInfo;
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    property SourceSelectQuery : String read FSourceSelectQuery write FSourceSelectQuery;
+    property SourceKeyFields : TStringList read FSourceKeyFields;
+    property DestinationSelectQuery : String read FDestinationSelectQuery write FDestinationSelectQuery;
+    property FieldsMapping : TDataReplicaFields read FFieldsMapping;
+    property DestinationTableName : String read FDestinationTableName write FDestinationTableName;
+
+    property SourceConnectionInfo: TmDatabaseConnectionInfo read FSourceConnectionInfo;
+    property DestinationConnectionInfo : TmDatabaseConnectionInfo read FDestinationConnectionInfo;
+  end;
+
+  { TDataReplicaTables }
+
+  TDataReplicaTables = class
+  strict private
+    FList : TObjectList;
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    function Count : integer;
+    function Add : TDataReplicaTableToTable;
+    function Get(const aIndex : integer): TDataReplicaTableToTable;
+  end;
+
+implementation
+uses
+  sysutils;
+
+{ TDataReplicaTables }
+
+constructor TDataReplicaTables.Create;
+begin
+  FList := TObjectList.Create(true);
+end;
+
+destructor TDataReplicaTables.Destroy;
+begin
+  FList.Free;
+  inherited Destroy;
+end;
+
+function TDataReplicaTables.Count: integer;
+begin
+  Result := FList.Count;
+end;
+
+function TDataReplicaTables.Add: TDataReplicaTableToTable;
+begin
+  Result := TDataReplicaTableToTable.Create;
+  FList.Add(Result);
+end;
+
+function TDataReplicaTables.Get(const aIndex: integer): TDataReplicaTableToTable;
+begin
+  Result := FList.Items[aIndex] as TDataReplicaTableToTable;
+end;
+
+{ TDataReplicaTableToTable }
+
+constructor TDataReplicaTableToTable.Create;
+begin
+  FSourceSelectQuery := '';
+  FSourceKeyFields := TStringList.Create;
+  FDestinationSelectQuery := '';
+  FFieldsMapping := TDataReplicaFields.Create;
+  FDestinationTableName:= '';
+  FSourceConnectionInfo:= TmDatabaseConnectionInfo.Create;
+  FDestinationConnectionInfo:= TmDatabaseConnectionInfo.Create;
+end;
+
+destructor TDataReplicaTableToTable.Destroy;
+begin
+  FSourceKeyFields.Free;
+  FFieldsMapping.Free;
+  FSourceConnectionInfo.Free;
+  FDestinationConnectionInfo.Free;
+  inherited Destroy;
+end;
+
+{ TDataReplicaFields }
+
+constructor TDataReplicaFields.Create;
+begin
+  FList := TObjectList.Create(true);
+  FIndexBySourceFieldName := TmStringDictionary.Create(false);
+end;
+
+destructor TDataReplicaFields.Destroy;
+begin
+  FList.Free;
+  FIndexBySourceFieldName.Free;
+  inherited Destroy;
+end;
+
+procedure TDataReplicaFields.RebuildIndexes;
+var
+  i : integer;
+begin
+  FIndexBySourceFieldName.Clear;
+  for i := 0 to Count -1 do
+    FIndexBySourceFieldName.Add(Get(i).SourceField.AsUppercaseString, Get(i));
+end;
+
+function TDataReplicaFields.Count: integer;
+begin
+  Result := FList.Count;
+end;
+
+function TDataReplicaFields.Get(const aIndex: integer): TDataReplicaFieldToField;
+begin
+  Result := FList.Items[aIndex] as TDataReplicaFieldToField;
+end;
+
+function TDataReplicaFields.Add: TDataReplicaFieldToField;
+begin
+  Result := TDataReplicaFieldToField.Create;
+  FList.Add(Result);
+end;
+
+function TDataReplicaFields.GetBySourceFieldName(const aSourceFieldName: String): TDataReplicaFieldToField;
+begin
+  if FIndexBySourceFieldName.Count = 0 then
+    RebuildIndexes;
+  Result := FIndexBySourceFieldName.Find(Uppercase(aSourceFieldName)) as TDataReplicaFieldToField;
+end;
+
+{ TDataReplicaFieldToField }
+
+constructor TDataReplicaFieldToField.Create;
+begin
+  FSourceField := TNullableString.Create();
+  FDestinationField := TNullableString.Create();
+end;
+
+destructor TDataReplicaFieldToField.Destroy;
+begin
+  FSourceField.Free;
+  FDestinationField.Free;
+  inherited Destroy;
+end;
+
+end.
