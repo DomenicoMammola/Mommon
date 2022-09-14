767cc8f7bec4d929e88b5534060d5524e5920f5a
Domenico Mammola <mimmo71@gmail.com>
2022-09-14 16:58:40 +0200

    * optimized GenerateDisplayLabel

--- src/dataset/mDatasetStandardSetup.pas
+++ src/dataset/mDatasetStandardSetup.pas
@@ -30,11 +30,49 @@ uses
   mFields;
 
 function GenerateDisplayLabel(aSourceString: String): String;
+var
+  i : integer;
+  curChar : Char;
+  doCapitalize : boolean;
 begin
+  Result := '';
+  doCapitalize := true;
+  for i := 1 to Length(aSourceString) do
+  begin
+    curChar := aSourceString[i];
+    case curChar of
+      '_', #9, #13, #10:
+        begin
+          Result := Result + ' ';
+          doCapitalize:= true;
+        end;
+      SEPARATOR_FIELDS_FROM_JOIN, SEPARATOR_FIELDS_FROM_INTERNAL_REFERENCE :
+        begin
+          Result := Result + '.';
+          doCapitalize := true;
+        end;
+      else
+        begin
+          if doCapitalize then
+          begin
+            doCapitalize:= false;
+            Result := Result + UpperCase(curChar);
+          end
+          else
+          begin
+            Result := Result + LowerCase(curChar);
+            if curChar = '\' then
+              doCapitalize := true;
+          end;
+        end;
+    end;
+  end;
+  (*
   Result := SysUtils.StringReplace(aSourceString, '_', ' ', [rfReplaceAll]);
   Result := SysUtils.StringReplace(Result, SEPARATOR_FIELDS_FROM_JOIN, '.', [rfReplaceAll]);
   Result := SysUtils.StringReplace(Result, SEPARATOR_FIELDS_FROM_INTERNAL_REFERENCE, '.', [rfReplaceAll]);
   Result := StrUtils.AnsiPropercase(Result,[' ', #9, '\', #13, #10, '.']);
+  *)
 end;
 
 procedure ApplyStandardSettingsToFields(aDataset: TDataset; aStandardFloatFormat : String);
