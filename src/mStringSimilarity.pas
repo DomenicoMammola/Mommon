413accb111afa8fbf6547d11cd3a0d9c683fd153
Domenico Mammola <mimmo71@gmail.com>
2022-09-19 18:26:15 +0200

    * huge improvement to the algorithm that computes a similarity score against two strings

--- src/mStringSimilarity.pas
+++ src/mStringSimilarity.pas
@@ -18,23 +18,23 @@ interface
 uses
   Classes;
 
-// Simon White algorithm
-// http://www.catalysoft.com/articles/StrikeAMatch.html
-function CompareStrings (aStr1, aStr2 : String; const aIgnorePunctuation : boolean) : double;
+// Inspired by Simon White algorithm http://www.catalysoft.com/articles/StrikeAMatch.html
+// but with some changes:
+// * the nested loops *must* be "bigger" than "smaller", if not you can get results higher than 1 and the result depends on the order of the two string parameters
+// * as the original algorithm does not work well with strings like "PAUL" against "PAULA", added an extra algorithm which checks similar chars in similar relative positions
+function CompareStrings (const aStr1, aStr2 : String; const aIgnorePunctuation : boolean) : double;
 
 implementation
 
 uses
-  SysUtils;
+  SysUtils, Math;
 
 procedure LetterPairs (const aStr : String; aListOfPairs : TStringList);
 var
   i : integer;
 begin
   for i := 1 to Length(aStr) - 1 do
-  begin
     aListOfPairs.Add(Copy(aStr, i, 2));
-  end;
 end;
 
 procedure WordLetterPairs(const aStr : String; aListOfPairs : TStringList);
@@ -75,11 +75,59 @@ begin
     Result := tmp;
 end;
 
-function CompareStrings (aStr1, aStr2 : String; const aIgnorePunctuation : boolean) : double;
+function MatchChars (const aStr1, aStr2 : String): double;
+var
+  lun1, lun2, bigLength, smallLength : integer;
+  bigString, smallString : String;
+  width, p, i, k, matching : integer;
+  match : boolean;
+begin
+  Result := 0;
+
+  lun1 := Length(aStr1);
+  lun2 := Length(aStr2);
+
+  if (lun1 = 0) or (lun2 = 0) then
+    exit;
+
+  if lun1 < lun2 then
+  begin
+    smallString := aStr1;
+    bigString := aStr2;
+    smallLength := lun1;
+    bigLength := lun2;
+  end
+  else
+  begin
+    smallString := aStr2;
+    bigString := aStr1;
+    smallLength := lun2;
+    bigLength := lun1;
+  end;
+
+  width := bigLength div smallLength;
+  matching := 0;
+  for i := 1 to smallLength do
+  begin
+    p := (i * width) - 1;
+    match := false;
+    for k := p to p + 2 do
+    begin
+      if (k >= 1) and (k <= bigLength) then
+        match := match or (CompareText(bigString[k], smallString[i]) = 0);
+    end;
+    if match then
+      inc(matching);
+  end;
+
+  Result := (smallLength / bigLength) - ((smallLength - matching) * (1 / bigLength));
+end;
+
+function CompareStrings (const aStr1, aStr2 : String; const aIgnorePunctuation : boolean) : double;
 var
   tmpList1, tmpList2 : TStringList;
-  intersection, union : integer;
-  i, j : integer;
+  intersection, increment, penalty : double;
+  i, j, l1, l2: integer;
   tmpStr1, tmpStr2 : String;
 begin
   tmpStr1 := AdjustString(aStr1, aIgnorePunctuation);
@@ -104,30 +152,49 @@ begin
   tmpList1 := TStringList.Create;
   tmpList2 := TStringList.Create;
   try
-    WordLetterPairs(tmpStr1, tmpList1);
-    WordLetterPairs(tmpStr2, tmpList2);
+    if Length(tmpStr1) >= Length(tmpStr2) then
+    begin
+      WordLetterPairs(tmpStr1, tmpList1);
+      WordLetterPairs(tmpStr2, tmpList2);
+    end
+    else
+    begin
+      WordLetterPairs(tmpStr2, tmpList1);
+      WordLetterPairs(tmpStr1, tmpList2);
+    end;
     intersection := 0;
-    union := tmpList1.Count + tmpList2.Count;
+    //union := tmpList1.Count + tmpList2.Count;
+    l1 := tmpList1.Count;
+    l2 := tmpList2.Count;
 
+    penalty := 0;
     for i := 0 to tmpList1.Count - 1 do
     begin
       j := 0;
+      increment := 1 - penalty;
       while j < tmpList2.Count do
       begin
         if CompareText(tmpList1.Strings[i], tmpList2.Strings[j]) = 0 then
         begin
-          inc (intersection);
+          intersection := intersection + increment;
+          increment := 1;
           tmpList2.Delete(j);
         end
         else
+        begin
           inc(j);
+          increment := increment - (1 / l2);
+          penalty := penalty + (1 / (l1 * l2 * 3));
+        end;
       end;
     end;
   finally
     tmpList1.Free;
     tmpList2.Free;
   end;
-  Result := (2.0 * intersection) / union;
+  Result := (1 / Max(l1, l2)) * intersection;
+  Result := Max(Result, MatchChars(tmpStr1, tmpStr2));
+//  Result := ((2.0 * intersection) / union);
 end;
 
 end.
