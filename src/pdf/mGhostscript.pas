8bd1f173e3aed3c528ec514dbf2aaab6aeb313f9
Domenico Mammola <mimmo71@gmail.com>
2022-12-30 17:02:45 +0100

    * add procedure to convert a pdf to PDF/A-1 and PDF/A-2

--- src/pdf/mGhostscript.pas
+++ src/pdf/mGhostscript.pas
@@ -24,6 +24,7 @@ resourcestring
   SGhostscript_error_unable_to_run = 'Unable to run gs: %s';
 
 type
+  TPDFAConformity = (pdfa1, pdfa2);
 
   { TGhostscriptToolbox }
 
@@ -32,6 +33,7 @@ type
     class function CheckGhostscript_ExePath : boolean;
   public
     class function CompressPdf(const aPdfFileName, aDestinationFileName : string; const aResolution : integer = 150) : boolean;
+    class function ConvertToPDFA(const aPdfFileName, aDestinationFileName : string; const aConformity : TPDFAConformity) : boolean;
     class function GetLastError : String;
   end;
 
@@ -73,6 +75,33 @@ begin
   // -sDEVICE=pdfwrite -dCompatabilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dBATCH -dColorImageResolution=150 -sOutputFile="pippo.pdf" "TCL_R51M -EU_EN C635.pdf"
   if RunCommandIndir(ExtractFileDir(aDestinationFileName), Ghostscript_gs_ExePath, ['-sDEVICE=pdfwrite', '-dCompatabilityLevel=1.4',
       '-dPDFSETTINGS=/ebook', '-dNOPAUSE', '-dBATCH', '-dColorImageResolution=' + IntToStr(aResolution),  '-sOutputFile=' + AnsiQuotedStr(ExtractFileName(aDestinationFileName), '"'), AnsiQuotedStr(aPdfFileName, '"')],
+      outputString, [poNoConsole, poWaitOnExit, poStderrToOutPut]) then
+    Result := true
+  else
+    FLastError := Format(SGhostscript_error_unable_to_run, [outputString]);
+end;
+
+class function TGhostscriptToolbox.ConvertToPDFA(const aPdfFileName, aDestinationFileName: string; const aConformity : TPDFAConformity): boolean;
+var
+  outputString, conf : string;
+begin
+  Result := false;
+  if not CheckGhostscript_ExePath then
+    exit;
+
+  if not FileExists(aPdfFileName) then
+  begin
+    FLastError := Format(SGhostscript_error_file_missing, [aPdfFileName]);
+    exit;
+  end;
+
+  if aConformity = pdfa2 then
+    conf := '-dPDFA=2'
+  else
+    conf := '-dPDFA';
+
+  if RunCommandIndir(ExtractFileDir(aDestinationFileName), Ghostscript_gs_ExePath, ['-sDEVICE=pdfwrite',
+      conf, '-dNOPAUSE', '-dBATCH', '-sColorConversionStrategy=UseDeviceIndependentColor', '-dPDFACompatibilityPolicy=1', '-sOutputFile=' + AnsiQuotedStr(ExtractFileName(aDestinationFileName), '"'), AnsiQuotedStr(aPdfFileName, '"')],
       outputString, [poNoConsole, poWaitOnExit, poStderrToOutPut]) then
     Result := true
   else
