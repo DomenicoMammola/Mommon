e9a5c1ed28c6f7b88d9e50d9e36ff3b9544358db
DomenicoMammola <mimmo71@gmail.com>
2023-06-15 17:26:06 +0200

    * more power to the function CompareStrings

--- src/mStringSimilarity.pas
+++ src/mStringSimilarity.pas
@@ -22,12 +22,16 @@ uses
 // but with some changes:
 // * the nested loops *must* be "bigger" than "smaller", if not you can get results higher than 1 and the result depends on the order of the two string parameters
 // * as the original algorithm does not work well with strings like "PAUL" against "PAULA", added an extra algorithm which checks similar chars in similar relative positions
-function CompareStrings (const aStr1, aStr2 : String; const aIgnorePunctuation : boolean) : double;
+function CompareStrings (const aStr1, aStr2 : String; const aIgnorePunctuation : boolean; const aCheckScrambledWords : boolean = false) : double;
 
 implementation
 
 uses
+  mMathUtility,
   SysUtils, Math;
+
+
+const punctuationChars : set of Char = ['.', ':', '?', '!', '_', '^', '-', '\', '/', ';', ',', '''', '"'];
 
 procedure LetterPairs (const aStr : String; aListOfPairs : TStringList);
 var
@@ -67,7 +71,7 @@ begin
     Result := '';
     for i := 1 to Length(tmp) do
     begin
-      if not (tmp[i] in ['.', ':', '?', '!', '_', '^', '-', '\', '/', ';', ',', '''', '"']) then
+      if not (tmp[i] in punctuationChars) then
         Result := Result + tmp[i];
     end;
   end
@@ -123,30 +127,110 @@ begin
   Result := (smallLength / bigLength) - ((smallLength - matching) * (1 / bigLength));
 end;
 
-function CompareStrings (const aStr1, aStr2 : String; const aIgnorePunctuation : boolean) : double;
+procedure SeparateWords (const aStr : String; aWordsList : TStringList);
+var
+  k : integer;
+  lastWord : String;
+begin
+  lastWord := '';
+
+  for k := 1 to Length(aStr) do
+  begin
+    if (aStr[k] in punctuationChars) or (aStr[k] = ' ') then
+    begin
+      if lastWord <> '' then
+      begin
+        aWordsList.Add(lastWord);
+        lastWord := '';
+      end;
+    end
+    else
+      lastWord := lastWord + aStr[k];
+  end;
+  if lastWord <> '' then
+    aWordsList.Add(lastWord);
+end;
+
+function MatchScrambledWords (const aStr1, aStr2: String) : double;
+var
+  list1, list2 : TStringList;
+  maxValue, step : double;
+  i, w, maxCount : integer;
+
+begin
+  Result := 0;
+  list1 := TStringList.Create;
+  list2 := TStringList.Create;
+  try
+    SeparateWords(aStr1, list1);
+    SeparateWords(aStr2, list2);
+
+    maxCount := max(list1.Count, list2.Count);
+    maxValue := 1 - ((1 / maxCount) * Abs(list1.Count - list2.Count));
+
+    step := maxValue / maxCount;
+
+
+    i := list1.Count - 1;
+    w := list2.Count - 1;
+
+    while (i >= 0) do
+    begin
+      if list1.Strings[i] = list2.Strings[w] then
+      begin
+        Result := Result + step;
+        list1.Delete(i);
+        list2.Delete(w);
+        dec(i);
+        dec(w);
+      end
+      else
+        dec(w);
+
+      if w < 0 then
+      begin
+        w := list2.Count - 1;
+        dec(i);
+      end;
+    end;
+
+    for i := 0 to min(list1.Count, list2.Count) - 1 do
+      Result := Result + (step * CompareStrings(list1.Strings[i], list2.Strings[i], false, false));
+
+    Result := Min(1, RoundToExt(Result, rmHalfRoundAwayFromZero, 6));
+  finally
+    list2.Free;
+    list1.Free;
+  end;
+end;
+
+function CompareStrings (const aStr1, aStr2 : String; const aIgnorePunctuation : boolean; const aCheckScrambledWords : boolean = false) : double;
 var
   tmpList1, tmpList2 : TStringList;
   intersection, increment, penalty : double;
   i, j, l1, l2: integer;
   tmpStr1, tmpStr2 : String;
 begin
+  Result := 0;
+
   tmpStr1 := AdjustString(aStr1, aIgnorePunctuation);
   tmpStr2 := AdjustString(aStr2, aIgnorePunctuation);
   if (trim(tmpStr1) = '') then
   begin
     if (trim(tmpStr2) = '') then
-      Result := 1
-    else
-      Result := 0;
+      Result := 1;
     exit;
   end
   else
   begin
     if (trim(tmpStr2) = '') then
-    begin
-      Result := 0;
       exit;
-    end;
+  end;
+
+  if tmpStr1 = tmpStr2 then
+  begin
+    Result := 1;
+    exit;
   end;
 
   tmpList1 := TStringList.Create;
@@ -192,9 +276,12 @@ begin
     tmpList1.Free;
     tmpList2.Free;
   end;
-  Result := (1 / Max(l1, l2)) * intersection;
+  if (l1 > 0) or (l2 > 0) then
+    Result := (1 / Max(l1, l2)) * intersection;
   Result := Max(Result, MatchChars(tmpStr1, tmpStr2));
-//  Result := ((2.0 * intersection) / union);
+
+  if aCheckScrambledWords then
+    Result := Max(Result, MatchScrambledWords(aStr1, aStr2));
 end;
 
 end.
--- tests/mStringSimilarity/testcase1.pas
+++ tests/mStringSimilarity/testcase1.pas
@@ -23,33 +23,39 @@ type
 
 implementation
 
-function CheckStrings(const aString1, aString2 : String; const aIgnorePunctuation : boolean): double;
+function CheckStrings(const aString1, aString2 : String; const aIgnorePunctuation, aCheckScrambledWords : boolean): double;
 var
   s : String;
 begin
-  Result := CompareStrings(aString1, aString2, aIgnorePunctuation);
+  Result := CompareStrings(aString1, aString2, aIgnorePunctuation, aCheckScrambledWords);
   s := '"' + aString1 + '" compared to "' + aString2 + '"';
   if aIgnorePunctuation then
     s := s + ' (no punctuation)';
+  if aCheckScrambledWords then
+    s := s + ' (check scrambled words)';
   WriteLn(s + ' = ' + FloatToStr(Result));
 end;
 
 procedure TTestCase1.TestStringSimilarity;
 begin
-  CheckEquals(1, CheckStrings('pippo', 'pippo', false));
-  CheckTrue(CheckStrings('pippo', 'pipppo', false) >= 0.8);
-  CheckTrue(CheckStrings('pippo', 'pippi', false) >= 0.8);
-  CheckTrue(CheckStrings('poppi', 'pipppo', false) >= 0.5);
-  CheckTrue(CheckStrings('ei fu siccome immobile', 'ei fu immobile siccome', false) >= 0.5);
-  CheckTrue(CheckStrings('ei fu siccome immobile', 'ei fu', false) < 0.3);
-  CheckTrue(CheckStrings('mldnasdkl asdkksjdf hkj dlfjksah', 'fjkjsàqòr we wioe skdfkajsdjk jldsfa k', false) < 0.3);
-  CheckTrue(CheckStrings('Paul', 'Paula', false) >= 0.7);
-  CheckTrue(CheckStrings('Orange is the new black', 'Black is the new orange', false) >= 0.5);
-  CheckTrue(CheckStrings('The, same, thing;', 'The same thing', true) = 1);
-  CheckTrue(CheckStrings('The, same, thing;', 'The same thing', false) >= 0.5);
-  CheckTrue(CheckStrings('missing some char', 'misin som chr', false) >= 0.5);
-  CheckTrue(CheckStrings('change correct order 1 2 3', 'change correct order 3 2 1', false) >= 0.75);
-  CheckTrue(CheckStrings('Boooooohhhh!', 'Booohhh!', false) >= 0.55);
+  CheckEquals(1, CheckStrings('pippo', 'pippo', false, false));
+  CheckTrue(CheckStrings('pippo', 'pipppo', false, false) >= 0.8);
+  CheckTrue(CheckStrings('pippo', 'pippi', false, false) >= 0.8);
+  CheckTrue(CheckStrings('poppi', 'pipppo', false, false) >= 0.5);
+  CheckTrue(CheckStrings('ei fu siccome immobile', 'ei fu immobile siccome', false, false) >= 0.5);
+  CheckTrue(CheckStrings('ei fu siccome immobile', 'ei fu', false, false) < 0.3);
+  CheckTrue(CheckStrings('mldnasdkl asdkksjdf hkj dlfjksah', 'fjkjsàqòr we wioe skdfkajsdjk jldsfa k', false, false) < 0.3);
+  CheckTrue(CheckStrings('Paul', 'Paula', false, false) >= 0.7);
+  CheckTrue(CheckStrings('Orange is the new black', 'Black is the new orange', false, false) >= 0.5);
+  CheckTrue(CheckStrings('The, same, thing;', 'The same thing', true, false) = 1);
+  CheckTrue(CheckStrings('The, same, thing;', 'The same thing', false, true) = 1);
+  CheckTrue(CheckStrings('The, same, thing;', 'The same thing', false, false) >= 0.5);
+  CheckTrue(CheckStrings('missing some char', 'misin som chr', false, false) >= 0.5);
+  CheckTrue(CheckStrings('change correct order 1 2 3', 'change correct order 3 2 1', false, false) >= 0.75);
+  CheckTrue(CheckStrings('change correct order 1 2 3', 'change correct order 3 2 1', false, true) = 1);
+  CheckTrue(CheckStrings('Boooooohhhh!', 'Booohhh!', false, false) >= 0.55);
+  CheckTrue(CheckStrings('AGRINTESA SOC. COOP. AGRICOLA', 'AGRINTESA SOC.AGR.COOP.', false, false) >= 0.6);
+  CheckTrue(CheckStrings('AGRINTESA SOC. COOP. AGRICOLA', 'AGRINTESA SOC.AGR.COOP.', false, true) >= 0.8);
 end;
 
 
